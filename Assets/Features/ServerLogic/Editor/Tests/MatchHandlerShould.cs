002ea51fc958a5b3976f04dfd629599c38e40dae
DonForse <maxi_rejlis@hotmail.com>
2022-12-08 17:58:57 -0300

    Update MatchHandlerShould.cs

--- Assets/Features/ServerLogic/Editor/Tests/MatchHandlerShould.cs
+++ Assets/Features/ServerLogic/Editor/Tests/MatchHandlerShould.cs
@@ -3,12 +3,14 @@ using Features.ServerLogic.Matches.Action;
 using Features.ServerLogic.Matches.Infrastructure;
 using Features.ServerLogic.Matches.Service;
 using Features.ServerLogic.Users.Infrastructure;
+using NSubstitute;
 using NUnit.Framework;
 
 namespace Features.ServerLogic.Editor.Tests
 {
     public class MatchHandlerShould
     {
+        private const string UserId = "UserId";
         private MatchHandler _matchHandler;
         private IUsersQueuedRepository _usersQueuedRepository;
         private IFriendsUsersQueuedRepository _friendsQueuedRepository;
@@ -20,13 +22,31 @@ namespace Features.ServerLogic.Editor.Tests
         [SetUp]
         public void Setup()
         {
+            _matchCreatorService = Substitute.For<IMatchCreatorService>();
             _matchHandler = new MatchHandler(_usersQueuedRepository, _friendsQueuedRepository, _matchesRepository, _usersRepository, _matchCreatorService,_createMatch );
         }
 
         [Test]
         public void CreateMatchesWhenGet()
         {
-            
+            _matchHandler.Get(UserId);
+            _matchCreatorService.Received(1).CreateMatches();
+        }
+        
+        [Test]
+        public void ReturnNullWhenGetAndNoMatch()
+        {
+            _matchHandler.Get(UserId);
+            _matchCreatorService.Received(1).CreateMatches();
+            Assert.Fail();
+        }
+        
+        [Test]
+        public void ReturnMatchWhenGetAndMatchExists()
+        {
+            _matchHandler.Get(UserId);
+            _matchCreatorService.Received(1).CreateMatches();
+            Assert.Fail();
         }
     }
 }
