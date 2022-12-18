0d85cb39d0bd4e688e07090c718dae8116dd92dd
DonForse <maxi_rejlis@hotmail.com>
2022-12-18 10:20:11 -0300

    start adding user handler tests

---
+++ Assets/Features/ServerLogic/Editor/Tests/UserHandlerShould.cs
@@ -0,0 +1,22 @@
+﻿using Features.ServerLogic.Handlers;
+using NUnit.Framework;
+
+namespace Features.ServerLogic.Editor.Tests
+{
+    public class UserHandlerShould
+    {
+        private UserHandler _userHandler;
+
+        [SetUp]
+        public void Setup()
+        {
+            //_userHandler = new UserHandler();
+        }
+
+        [Test]
+        public void Test()
+        {
+            Assert.Fail();
+        }
+    }
+}
