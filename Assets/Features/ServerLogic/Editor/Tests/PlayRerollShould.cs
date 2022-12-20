7ef1d3d87b845fc8879ae59f16d51e5ea89a3f3f
DonForse <maxi_rejlis@hotmail.com>
2022-12-19 23:31:59 -0300

    add test classes for reroll and play unit

---
+++ Assets/Features/ServerLogic/Editor/Tests/PlayRerollShould.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace Features.ServerLogic.Editor.Tests
+{
+    public class PlayRerollShould
+    {
+        [Test]
+        public void Play()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ Assets/Features/ServerLogic/Editor/Tests/PlayUnitCardShould.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace Features.ServerLogic.Editor.Tests
+{
+    public class PlayUnitCardShould
+    {
+        [Test]
+        public void Play()
+        {
+            Assert.Fail();
+        }
+    }
+}
