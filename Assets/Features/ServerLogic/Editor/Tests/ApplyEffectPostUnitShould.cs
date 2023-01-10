eb6d67ef46e2fd2c1da984d26068404465fb13fc
DonForse <maxi_rejlis@hotmail.com>
2023-01-10 20:39:59 -0300

    add test class

---
+++ Assets/Features/ServerLogic/Editor/Tests/ApplyEffectPostUnitShould.cs
@@ -0,0 +1,25 @@
+﻿using Features.ServerLogic.Matches.Action;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Features.ServerLogic.Editor.Tests
+{
+    public class ApplyEffectPostUnitShould
+    {
+        private ApplyEffectPostUnit _applyEffectPostUnit;
+        private IGetMatch _getMatch;
+
+        [SetUp]
+        public void Setup()
+        {
+            _getMatch = Substitute.For<IGetMatch>();
+            _applyEffectPostUnit = new ApplyEffectPostUnit(_getMatch);
+        }
+
+        [Test]
+        public void Asd()
+        {
+            Assert.Fail();
+        }
+    }
+}
