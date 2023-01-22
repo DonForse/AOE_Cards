e60f6198ead7b12376433ff045f1d36c0cbfc46d
Maximiliano Rejlis <maximiliano.rejlis@etermax.com>
2023-01-22 19:35:06 -0300

    inmemory repository card cleanup

--- Assets/Features/ServerLogic/Cards/Infrastructure/InMemoryCardRepository.cs
+++ Assets/Features/ServerLogic/Cards/Infrastructure/InMemoryCardRepository.cs
@@ -10,35 +10,35 @@ namespace Features.ServerLogic.Cards.Infrastructure
 {
     public class InMemoryCardRepository : ICardRepository
     {
-        private List<UnitCard> UnitCards;
-        private List<UpgradeCard> UpgradeCards;
+        private List<UnitCard> _unitCards;
+        private List<UpgradeCard> _upgradeCards;
+
         public IList<UnitCard> GetUnitCards()
         {
-            if (UnitCards == null)
-                UnitCards = LoadUnitCards();
-
-            return UnitCards.ToList();
+            _unitCards ??= LoadUnitCards();
+            return _unitCards.ToList();
         }
+
         public IList<UpgradeCard> GetUpgradeCards()
         {
-            if (UpgradeCards == null)
-                UpgradeCards = LoadUpgradeCards();
-            return UpgradeCards.ToList();
+            _upgradeCards ??= LoadUpgradeCards();
+            return _upgradeCards.ToList();
         }
 
         public UnitCard GetUnitCard(string cardName)
         {
             cardName = cardName.ToLower();
-            var card = UnitCards.FirstOrDefault(uc => uc.CardName.ToLower() == cardName);
+            var card = _unitCards.FirstOrDefault(uc => uc.CardName.ToLower() == cardName);
             return card;
         }
 
         public UpgradeCard GetUpgradeCard(string cardName)
         {
             cardName = cardName.ToLower();
-            var card = UpgradeCards.FirstOrDefault(uc => uc.CardName.ToLower() == cardName);
+            var card = _upgradeCards.FirstOrDefault(uc => uc.CardName.ToLower() == cardName);
             return card;
         }
+
         private List<UpgradeCard> LoadUpgradeCards()
         {
             var cards = new List<UpgradeCard>();
@@ -50,92 +50,20 @@ namespace Features.ServerLogic.Cards.Infrastructure
                 UpgradeCard card = LoadUpgradeCard(data);
                 cards.Add(card);
             }
+
             return cards;
         }
 
         private UpgradeCard LoadUpgradeCard(string[] data)
         {
-            switch (data[3].ToLower())
-            {
-                case "furor celtica":
-                    return SetFurorCelticaUpgradeCard(data);
-                case "persian town center":
-                    return SetPersianTCUpgradeCard(data);
-                case "madrasah":
-                    return SetMadrasahUpgradeCard(data);
-                case "teutons faith":
-                    return SetTeutonsFaithUpgradeCard(data);
-                default:
-                    return SetDefaultUpgradeCard(data);
-            }
-        }
-
-        private UpgradeCard SetTeutonsFaithUpgradeCard(string[] data)
-        {
             var archetypes = data[2].Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries);
             var archetypesVs = data[5].Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries);
             var card = new UpgradeCard()
             {
                 CardName = data[3],
                 BasePower = int.Parse(data[4]),
-                Archetypes = archetypes.Select(c => ConvertArchetype(c)).ToList(),
-                BonusVs = archetypesVs.Select(c => ConvertArchetype(c)).ToList(),
-            };
-            return card;
-        }
-
-        private UpgradeCard SetMadrasahUpgradeCard(string[] data)
-        {
-            var archetypes = data[2].Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries);
-            var archetypesVs = data[5].Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries);
-            var card = new UpgradeCard()
-            {
-                CardName = data[3],
-                BasePower = int.Parse(data[4]),
-                Archetypes = archetypes.Select(c => ConvertArchetype(c)).ToList(),
-                BonusVs = archetypesVs.Select(c => ConvertArchetype(c)).ToList(),
-            };
-            return card;
-        }
-
-        private UpgradeCard SetPersianTCUpgradeCard(string[] data)
-        {
-            var archetypes = data[2].Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries);
-            var archetypesVs = data[5].Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries);
-            var card = new UpgradeCard()
-            {
-                CardName = data[3],
-                BasePower = int.Parse(data[4]),
-                Archetypes = archetypes.Select(c => ConvertArchetype(c)).ToList(),
-                BonusVs = archetypesVs.Select(c => ConvertArchetype(c)).ToList(),
-            };
-            return card;
-        }
-
-        private UpgradeCard SetFurorCelticaUpgradeCard(string[] data)
-        {
-            var archetypes = data[2].Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries);
-            var archetypesVs = data[5].Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries);
-            var card = new UpgradeCard()
-            {
-                CardName = data[3],
-                BasePower = int.Parse(data[4]),
-                Archetypes = archetypes.Select(c => ConvertArchetype(c)).ToList(),
-                BonusVs = archetypesVs.Select(c => ConvertArchetype(c)).ToList(),
-            };
-            return card;
-        }
-
-        private UpgradeCard SetDefaultUpgradeCard(string[] data)
-        {
-            var archetypes = data[2].Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries);
-            var archetypesVs = data[5].Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries);
-            var card = new UpgradeCard()
-            {
-                CardName = data[3],
-                BasePower = int.Parse(data[4]),
-                Archetypes = archetypes.Select(c => ConvertArchetype(c)).ToList(),
-                BonusVs = archetypesVs.Select(c => ConvertArchetype(c)).ToList(),
+                Archetypes = archetypes.Select(ConvertArchetype).ToList(),
+                BonusVs = archetypesVs.Select(ConvertArchetype).ToList(),
             };
             return card;
         }
@@ -144,42 +72,21 @@ namespace Features.ServerLogic.Cards.Infrastructure
         {
             var cards = new List<UnitCard>();
             var ta = Resources.Load<TextAsset>("units");
-            foreach (var line in ta.text.Split('\n')) //'\n' en mac
+            var lines = ta.text.Split('\n');
+            foreach (var line in lines) //'\n' en mac
             {
                 var data = line.Split(',');
-                UnitCard card = LoadUnitCard(data);
-                cards.Add(card); 
+                var card = LoadUnitCard(data);
+                cards.Add(card);
             }
+
             return cards;
         }
 
         private UnitCard LoadUnitCard(string[] data)
         {
-            if (data[0].ToLower() == "villager")
-                return SetVillagerUnitCard(data);
-
-            return SetDefaultUnitCard(data);
-        }
-
-        private UnitCard SetVillagerUnitCard(string[] data)
-        {
             var archetypes = data[2].Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries);
             var archetypesvs = data[5].Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries);
-            var card = new UnitCard()
-            {
-                CardName = data[0],
-                BasePower = int.Parse(data[3]),
-                Archetypes = archetypes.Select(c => ConvertArchetype(c)).ToList(),
-                BonusVs = archetypesvs.Select(c => ConvertArchetype(c)).ToList(),
-                PowerEffect = int.Parse(data[6])
-            };
-            return card;
-        }
-
-        private UnitCard SetDefaultUnitCard(string[] data)
-        {
-            var archetypes = data[2].Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries);
-            var archetypesvs = data[5].Split(new char[] { '|'}, StringSplitOptions.RemoveEmptyEntries);
             var card = new UnitCard()
             {
                 CardName = data[0].Trim('\n'),
@@ -193,37 +100,23 @@ namespace Features.ServerLogic.Cards.Infrastructure
 
         private Archetype ConvertArchetype(string c)
         {
-            switch (c)
+            return c switch
             {
-                case "Archer":
-                    return Archetype.Archer;
-                case "Camel":
-                    return Archetype.Camel;
-                case "Cavalry":
-                    return Archetype.Cavalry;
-                case "Cavalry Archer":
-                    return Archetype.CavalryArcher;
-                case "Counter Unit":
-                    return Archetype.CounterUnit;
-                case "Eagle Warrior":
-                case "Eagle":
-                    return Archetype.EagleWarrior;
-                case "Elephant":
-                    return Archetype.Elephant;
-                case "Militia":
-                    return Archetype.Militia;
-                case "Monk":
-                    return Archetype.Monk;
-                case "Siege Unit":
-                    return Archetype.SiegeUnit;
-                case "Infantry":
-                    return Archetype.Infantry;
-                case "Villager":
-                    return Archetype.Villager;
-                default:
-                    return Archetype.Villager;
-            }
+                "Archer" => Archetype.Archer,
+                "Camel" => Archetype.Camel,
+                "Cavalry" => Archetype.Cavalry,
+                "Cavalry Archer" => Archetype.CavalryArcher,
+                "Counter Unit" => Archetype.CounterUnit,
+                "Eagle Warrior" => Archetype.EagleWarrior,
+                "Eagle" => Archetype.EagleWarrior,
+                "Elephant" => Archetype.Elephant,
+                "Militia" => Archetype.Militia,
+                "Monk" => Archetype.Monk,
+                "Siege Unit" => Archetype.SiegeUnit,
+                "Infantry" => Archetype.Infantry,
+                "Villager" => Archetype.Villager,
+                _ => Archetype.Villager
+            };
         }
-
     }
 }
